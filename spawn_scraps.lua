b962417dda491fd9a64ae2b36deeb4de813155c9
q1qe <119639926+q1qe@users.noreply.github.com>
2024-07-24 22:36:02 +0700

    Create spawn_scraps.lua

---
+++ spawn_scraps.lua
@@ -0,0 +1,12 @@
+-- // Configuration
+local DropAmount = 100 -- // DO NOT GO OVER 1999, YOU WILL GET BANNED
+
+-- // Services
+local Workspace = game:GetService("Workspace")
+
+-- // Vars
+local ServerStuff = Workspace.ServerStuff
+local dropAmmo = ServerStuff.dropAmmo
+
+-- // Drop scrap (pick it up after)
+dropAmmo:FireServer("scrap", DropAmount)
